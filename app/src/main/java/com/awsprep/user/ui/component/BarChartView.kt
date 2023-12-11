e4e64fc80164d10a0b358e8e467b9c9adcc66b1b
noweshed-misl <nowshed@mirinfosys.com>
2023-12-11 19:08:30 +0600

    dynamic test summary

--- app/src/main/java/com/awsprep/user/ui/component/BarChartView.kt
+++ app/src/main/java/com/awsprep/user/ui/component/BarChartView.kt
@@ -1,12 +1,12 @@
 package com.awsprep.user.ui.component
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import co.yml.charts.axis.AxisData
-import co.yml.charts.common.model.AccessibilityConfig
 import co.yml.charts.common.model.Point
 import co.yml.charts.ui.barchart.BarChart
 import co.yml.charts.ui.barchart.models.BarChartData
@@ -27,52 +27,58 @@ fun BarChartView(
     val yStepSize = 5
     val barData = mutableListOf<BarData>()
 
-    for (i in 1..5) {
+    resultList.forEachIndexed { i, result ->
         barData += BarData(
             point = Point(
-                i.toFloat(),
-                (10..55).random().toFloat()
+                (i + 1).toFloat(),
+                result.correctAnswered.toFloat()
             ),
             color = PrimaryColor,
-            label = "Test $i",
+            label = result.testName,
         )
     }
 
-    val xAxisData = AxisData.Builder()
-        .axisStepSize(56.dp)
-        .steps(barData.size)
-        .bottomPadding(12.dp)
-        .axisLabelAngle(12f)
-        .startDrawPadding(32.dp)
-        .labelData { index -> barData[index].label }
-        .build()
+    Log.d("BarChartView: ", barData.toString())
 
-    val yAxisData = AxisData.Builder()
-        .steps(yStepSize)
-        .labelAndAxisLinePadding(24.dp)
-        .axisOffset(24.dp)
-        .labelData { index -> (index * (maxRange / yStepSize)).toString() }
-        .build()
+    if (barData.size > 0) {
 
-    val barChartData = BarChartData(
-        chartData = barData,
-        xAxisData = xAxisData,
-        yAxisData = yAxisData,
-        barStyle = BarStyle(
-            cornerRadius = 0.dp,
-            paddingBetweenBars = 36.dp,
-            barWidth = 24.dp
-        ),
-        showYAxis = true,
-        showXAxis = true,
-        horizontalExtraSpace = 24.dp
-    )
+        val xAxisData = AxisData.Builder()
+            .axisStepSize(56.dp)
+            .steps(barData.size)
+            .bottomPadding(12.dp)
+            .axisLabelAngle(12f)
+            .startDrawPadding(32.dp)
+            .shouldDrawAxisLineTillEnd(true)
+            .labelData { index -> barData[index].label }
+            .build()
 
-    BarChart(
-        modifier = Modifier
-            .height(300.dp)
-            .fillMaxWidth(),
-        barChartData = barChartData
-    )
+        val yAxisData = AxisData.Builder()
+            .steps(yStepSize)
+            .labelAndAxisLinePadding(24.dp)
+            .axisOffset(24.dp)
+            .labelData { index -> (index * (maxRange / yStepSize)).toString() }
+            .build()
+
+        val barChartData = BarChartData(
+            chartData = barData,
+            xAxisData = xAxisData,
+            yAxisData = yAxisData,
+            barStyle = BarStyle(
+                cornerRadius = 0.dp,
+                paddingBetweenBars = 36.dp,
+                barWidth = 24.dp
+            ),
+            showYAxis = true,
+            showXAxis = true,
+            horizontalExtraSpace = 24.dp
+        )
+
+        BarChart(
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            barChartData = barChartData
+        )
+    }
 
 }
